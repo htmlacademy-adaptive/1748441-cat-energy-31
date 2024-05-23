09702faf1b07c1f759a0b677cbc0661c773b3344
Olga <genntanaka@mail.ru>
2024-05-23 10:52:40 +0700

    Module6-task1 Сетки (#8)
    * Добавляет файловую структуру
    
    * подключает шрифты
    
    * Базовая стилизация
    
    * Исправляет шрифт и убирает лишные классы в файлах препроцессора
    
    * Исправления по stylelint
    
    * Исправления по линтеру
    
    * Мелкие правки
    
    * Исправления по линтеру
    
    * Добавляет векторную графику и исправляет line-height
    
    * Добавляет фоновые изображения и стили блокам hero, gift, program, logo
    
    * Добавляет эффекты hover, focus, active для векторных изображений
    
    * Сетка для меню и блока hero
    
    * Сетка для блока с примером и слайдер сравнения
    
    * Делает сравнение отдельным блоком
    
    * Сетки на главной для планшета
    
    * Сетки для планшета
    
    * Чинит по линтеру
    
    * Доработка сеток для tablet
    
    * Сетка для desctop
    
    * Исправления по линтеру
    
    * Правит паддинги у секций
    
    * Сетка для промоблока
    
    * Меняет позицию фонового кота в hero на десктопе
    
    * Лечит линтер
    
    * Меняет блок hero и некоторые сетки на главной
    
    * pixelperfect мобилка
    
    * Перенос изменений
    
    * Перенос изменений
    
    * Вся сетка
    
    * Чинит по линтеру

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+document.getElementById('toggle').onclick = openMenu;
+
+function openMenu() {
+  this.parentNode.classList.toggle('main-nav--open');
+  document.body.classList.toggle('page__body--open');
+}
