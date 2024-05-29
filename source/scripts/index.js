12bc3ef72177766f27f0309d06fc4bbbe5ef4737
Olga <genntanaka@mail.ru>
2024-05-29 17:45:17 +0700

    Final-task1 (#12)
    * Добавляет файловую структуру
    
    * подключает шрифты
    
    * Базовая стилизация
    
    * Исправляет шрифт и убирает лишные классы в файлах препроцессора
    
    * Исправления по stylelint
    
    * Исправления по линтеру
    
    * Мелкие правки
    
    * Исправления по линтеру
    
    * Добавляет векторную графику и исправляет line-height
    
    * Добавляет фоновые изображения и стили блокам hero, gift, program, logo
    
    * Добавляет эффекты hover, focus, active для векторных изображений
    
    * Сетка для меню и блока hero
    
    * Сетка для блока с примером и слайдер сравнения
    
    * Делает сравнение отдельным блоком
    
    * Сетки на главной для планшета
    
    * Сетки для планшета
    
    * Чинит по линтеру
    
    * Доработка сеток для tablet
    
    * Сетка для desctop
    
    * Исправления по линтеру
    
    * Правит паддинги у секций
    
    * Сетка для промоблока
    
    * Меняет позицию фонового кота в hero на десктопе
    
    * Лечит линтер
    
    * Меняет блок hero и некоторые сетки на главной
    
    * pixelperfect мобилка
    
    * Перенос изменений
    
    * Перенос изменений
    
    * Вся сетка
    
    * Чинит по линтеру
    
    * Добавляет декоративные элементы и интерактивную карту
    
    * Активный пунк меню
    
    * меняет блок hero
    
    * Меняет  контейнер
    
    * Исправление по линтеру
    
    * Меняет иконку pro_cat.svg
    
    * Добавляет webp на главную
    
    * Оптимизация
    
    * перенос изменений
    
    * Мелкие доработки
    
    * Переделывает пагинацию
    
    * Критерий Pixel Perfect
    
    * Меню без js, интерактивные элементы
    
    * Исправления по линтеру
    
    * Остальные критерии
    
    * Исправляет правку конфликтов
    
    * Исправляет блок hero
    
    * Блок с результатами изменений и состояния стрелок
    
    * Исправляет валидатор
    
    * Исправляет характеристики в живом примере
    
    * Исправляет характеристики в живом примере
    
    * Правки
    
    * Удаляет закоментированые стили
    
    * Правки
    
    * правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -7,3 +7,9 @@ function openMenu() {
   this.parentNode.classList.toggle('main-nav--open');
   document.body.classList.toggle('page__body--open');
 }
+
+const heroWrapper = document.getElementsByClassName('hero__wrapper')[0];
+const heightWrapper = document.querySelector('.hero').offsetHeight;
+const heightHeader = document.querySelector('.main-header').offsetHeight;
+const resultHeight = heightHeader + heightWrapper - 10;
+heroWrapper.style.setProperty('--height-before',`${resultHeight}px`);
